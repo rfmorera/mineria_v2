83b062ed55773bed2f3d1e3a40da00999a4cecef
Rafael Alejandro Fernandez Morera <rafael.fernandez@desoft.cu>
2020-05-22 17:44:10 -0400

    Report Model created

--- mineriaApp/Services/Utils/Enum.py
+++ mineriaApp/Services/Utils/Enum.py
@@ -3,3 +3,20 @@ from enum import Enum
 
 class InferenceModelsEnum(Enum):
     FastText = 1
+
+
+class TimeDeltaEnum(Enum):
+    Minuto1 = 1,
+    Minuto3 = 2,
+    Minuto5 = 3,
+    Minuto10 = 4,
+    Minuto15 = 5,
+    Minuto30 = 6,
+    Hora1 = 10,
+    Hora3 = 11,
+    Hora6 = 12,
+    Hora12 = 13,
+    Dia1 = 20,
+    Dia7 = 21,
+    Mes1 = 30,
+    Anno1 = 41
