e78628103a3aa2c48598db111378bcd8504287ce
Rafael Alejandro Fernandez Morera <rafael.fernandez@desoft.cu>
2020-06-17 18:17:00 -0400

    Feature/46 crear permisos via api

--- core/urls.py
+++ core/urls.py
@@ -17,12 +17,32 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from rest_framework.authtoken import views as viewsToken
-
+from django.contrib.auth import views as auth_views
 
 router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(
+        'admin/password_reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='admin_password_reset',
+    ),
+    path(
+        'admin/password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done',
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm',
+    ),
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete',
+    ),
     path('api/', include('rest_framework.urls', namespace='rest_framework')),
     path('auth-token', viewsToken.obtain_auth_token),
     path('v1/', include('mineriaApp.urls')),
--- mineriaApp/models.py
+++ mineriaApp/models.py
@@ -14,6 +14,13 @@ class Client(models.Model):
     nombre = models.CharField(max_length=255, null=False)
     descripcion = models.CharField(max_length=500, default="")
 
+    class Meta:
+        verbose_name = 'client'
+        verbose_name_plural = 'clients'
+
+    def __str__(self):
+        return self.nombre
+
 
 class User(AbstractUser):
     cliente = models.ForeignKey(Client, on_delete=models.CASCADE, default=1)
