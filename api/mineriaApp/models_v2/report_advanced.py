849edf884ea502d7d3566ab36d623dd1ee41b31e
Rafael Alejandro Fernandez Morera <rafael.fernandez@desoft.cu>
2021-01-12 18:58:15 -0500

    API endpoint CRUD

---
+++ api/mineriaApp/models_v2/report_advanced.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from django_mongoengine import Document
+from mongoengine import StringField, IntField, DateTimeField, ReferenceField, ListField
+
+from mineriaApp.models_v2.report_param import ReportParam
+
+
+class ReportAdvanced(Document):
+    client = IntField(required=True)
+    name = StringField(required=True)
+    description = StringField(default="")
+    basic_reports = ListField(ReferenceField(ReportParam))
+    created_on = DateTimeField(default=datetime.now())
+    meta = {'allow_inheritance': True}
+
+    @classmethod
+    def pre_save(cls, sender, document, **kwargs):
+        document.updated_on = datetime.now()
